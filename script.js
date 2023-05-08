0b76d431db40e3e1d456bf7a50795b13438a8445
Matt602 <matthew.frierson062@gmail.com>
2023-05-07 21:17:17 -0500

    Created some of the functions in the Javascript file as well as created a button to start/restart the game, and a div element to show the winner.

---
+++ script.js
@@ -0,0 +1,55 @@
+const Player = function(name, symbol) {
+    return { name, symbol}
+};
+
+const GameBoard = (() => {
+    let arr = ['O', 'X', 'O', 'X', 'O', 'X', 'X', 'O', 'X']
+    const checkGameOver = function(player1.symbol, player2.symbol) {
+        if( (arr[0] === arr[1] === arr[2]) )
+        {
+            return arr[0];
+        }
+        else if ( (arr[3] === arr[4] === arr[5]) )
+        {
+            return arr[3];
+        }
+        else if ( (arr[6] === arr[7] === arr[8]) )
+        {
+            return arr[6];
+        }
+        else if ( (arr[0] === arr[3] === arr[6]) )
+        {
+            return arr[0];
+        }
+        else if ( (arr[1] === arr[4] === arr[7]) )
+        {
+            return arr[1];
+        }
+        else if ( (arr[2] === arr[5] === arr[8]) )
+        {
+            return arr[2];
+        }
+        else if ( (arr[0] === arr[4] === arr[8]) )
+        {
+            return arr[0];
+        }
+        else if ( (arr[2] === arr[4] === arr[6]) )
+        {
+            return arr[2];
+        }
+        else 
+        {
+            return '';
+        }
+    };
+
+    const placeSymbol = function(symbol, pos) {
+        arr[pos] = symbol;
+    };
+
+    return { arr, checkGameOver, placeSymbol}
+})();
+
+const DisplayController = (() => {
+
+})();
